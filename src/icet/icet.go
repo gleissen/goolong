e4cb5f07ce6d445389da9ba3f03444e931c78199
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2018-04-17 15:12:42 -0700

    receive stmts

--- src/icet/icet.go
+++ src/icet/icet.go
@@ -7,7 +7,8 @@ import "fmt"
 import "log"
 
 type IceTVisitor struct {
-	IceTTerm string
+	currentProc string
+	IceTTerm    string
 }
 
 func main() {
@@ -15,7 +16,9 @@ func main() {
 }
 
 func makeNewIceTVisitor() *IceTVisitor {
-	v := &IceTVisitor{"skip"}
+	v := &IceTVisitor{"-1",
+		//make([]string, MAX_PROC_NUM),
+		"skip"}
 	return v
 }
 
@@ -34,34 +37,52 @@ func walkAst() {
 func (v *IceTVisitor) Visit(node ast.Node) (w ast.Visitor) {
 	switch node.(type) {
 	case *ast.CallExpr:
-		sendStr, ok := parseSend(node.(*ast.CallExpr))
+		// Send
+		sendStr, ok := parseSend(node.(*ast.CallExpr), v.currentProc)
 		if ok {
 			v.IceTTerm = fmt.Sprintf("%v;%v", sendStr, v.IceTTerm)
+		} else {
+			// New Node
+			proc, ok := parseNewNode(node.(*ast.CallExpr))
+			if ok {
+				v.currentProc = proc
+				v.IceTTerm = fmt.Sprintf("skip || %v", v.IceTTerm)
+			}
 		}
 	case *ast.AssignStmt:
-		recvString, ok := parseRecv(node.(*ast.AssignStmt))
+		// Recv
+		recvString, ok := parseRecv(node.(*ast.AssignStmt), v.currentProc)
 		if ok {
 			v.IceTTerm = fmt.Sprintf("%v;%v", recvString, v.IceTTerm)
 		}
-
 	}
 	return v
 }
 
-//fmt.Sprintf("at %v, %s", e.When, e.What)
-func parseSend(site *ast.CallExpr) (string, bool) {
+func parseNewNode(site *ast.CallExpr) (string, bool) {
 	sel, ok := site.Fun.(*ast.SelectorExpr)
 	if ok {
-		if sel.Sel.Name == "Send" {
-			arg1 := site.Args[0].(*ast.BasicLit).Value
-			arg2 := site.Args[1].(*ast.BasicLit).Value
-			return fmt.Sprintf("send(%v, %v)", arg1, arg2), true
+		if sel.Sel.Name == "CreateNewNode" {
+			proc := site.Args[0].(*ast.BasicLit).Value
+			return proc, true
 		}
 	}
 	return "", false
 }
 
-func parseRecv(assign *ast.AssignStmt) (string, bool) {
+func parseSend(site *ast.CallExpr, proc string) (string, bool) {
+	sel, ok := site.Fun.(*ast.SelectorExpr)
+	if ok {
+		if sel.Sel.Name == "Send" {
+			arg1 := site.Args[0].(*ast.BasicLit).Value
+			arg2 := site.Args[1].(*ast.BasicLit).Value
+			return fmt.Sprintf("send(%v, %v, %v)", proc, arg1, arg2), true
+		}
+	}
+	return "", false
+}
+
+func parseRecv(assign *ast.AssignStmt, proc string) (string, bool) {
 	if len(assign.Rhs) == 1 {
 		site, ok := assign.Rhs[0].(*ast.CallExpr)
 		if ok {
@@ -71,7 +92,7 @@ func parseRecv(assign *ast.AssignStmt) (string, bool) {
 				if ok {
 					if sel.Sel.Name == "Recv" && x.Name == "gochai" {
 						arg1 := assign.Lhs[0].(*ast.Ident).Name
-						return fmt.Sprintf("recv(%v)", arg1), true
+						return fmt.Sprintf("recv(%v,%v)", proc, arg1), true
 					}
 				}
 			}
