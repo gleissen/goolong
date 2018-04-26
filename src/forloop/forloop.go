87315bc96d0d671ba88256a57f0f0cadcc92f14d
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2018-04-26 15:48:27 -0700

    comments and property

--- src/forloop/forloop.go
+++ src/forloop/forloop.go
@@ -21,23 +21,27 @@ func main() {
 }
 
 func runServerProtocol(peerAddresses []string) {
-	n := gochai.CreateNewNode(0, *myAddr, peerAddresses, false)
+	n := gochai.CreateNewNode("m", 0, *myAddr, peerAddresses, false)
 	n.AssignSymSet("clients", "")
 	fmt.Println("Acting as server.")
 	//Protocol--
-	for id := range n.PeerIds {
-		fmt.Printf("sending 42 to %v\n", id)
-		n.Send(id, 42)
+	for ID := range n.PeerIds {
+		// {-@ invariant: forall([decl(id,int)], implies(elem(id,done), ref(msg, id)=42)) -@}
+		fmt.Printf("sending 42 to %v\n", ID)
+		val := gochai.NewIntVar(42)
+		n.Send(ID, val)
 	}
 	//--end
 }
 
 func runClientProtocol(peerAddresses []string) {
-	n := gochai.CreateNewNode(*myID, *myAddr, peerAddresses, true)
+	n := gochai.CreateNewNode("c", *myID, *myAddr, peerAddresses, true)
 	fmt.Println("Acting as client.")
 	// Protocol --
 	n.StartSymSet("clients", "p")
 	msg := n.Recv()
-	fmt.Printf("Received Message: %v", msg)
+	fmt.Printf("Received Message: %v", msg.Get())
 	// -- end
 }
+
+//{@- ensures: forall([decl(p,int)], implies(elem(p, clients), ref(msg, p)=42)) -@}
