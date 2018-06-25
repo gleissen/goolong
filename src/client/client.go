303f823f77a7afaf53381298b688b8c70bdd1135
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2018-06-25 16:12:05 -0700

    client

--- src/client/client.go
+++ src/client/client.go
@@ -24,6 +24,7 @@ var successful []int
 func main() {
 	flag.Parse()
 	replicaList := flag.Args()
+	N = len(replicaList)
 	if *conflicts > 100 {
 		log.Fatalf("Conflicts percentage must be between 0 and 100.\n")
 	}
@@ -45,74 +46,75 @@ func main() {
 		} else {
 			put[i] = false
 		}
-
-		//connecting to servers
-		servers := make([]net.Conn, N)
-		readers := make([]*bufio.Reader, N)
-		writers := make([]*bufio.Writer, N)
-		for i := 0; i < N; i++ {
-			var err error
-			servers[i], err = net.Dial("tcp", replicaList[i])
-			if err != nil {
-				log.Printf("Error connecting to replica %d\n", i)
-			}
-			readers[i] = bufio.NewReader(servers[i])
-			writers[i] = bufio.NewWriter(servers[i])
-		}
-
-		successful := make([]int, N)
-		leader := 0
-		done := make(chan bool, N)
-		var id int32 = 0
-		args := clientproto.Propose{CommandId: id, Command: state.Command{Op: state.PUT, K: 0, V: 0}}
-		before_total := time.Now()
-		// sending requests
-		for j := 0; j < *rounds; j++ {
-
-			n := *reqsNb / *rounds
-			for i := 0; i < n; i++ {
-
-				before := time.Now()
-				go waitReplies(readers, leader, n, done)
-				dlog.Printf("Sending proposal %d\n", id)
-				args.CommandId = id
-				if put[i] {
-					args.Command.Op = state.PUT
-				} else {
-					args.Command.Op = state.GET
-				}
-				args.Command.K = state.Key(karray[i])
-				args.Command.V = state.Value(i)
-
-				for rep := 0; rep < N; rep++ {
-					writers[rep].WriteByte(clientproto.PROPOSE)
-					args.Marshal(writers[rep])
-					writers[rep].Flush()
-				}
-				for i := 0; i < N; i++ {
-					writers[i].Flush()
-				}
-				err := <-done
-				after := time.Now()
-				fmt.Printf("Round took %v\n", after.Sub(before))
-				if err {
-					leader = (leader + 1) % N
-				}
-				id++
-			}
-		}
-		after_total := time.Now()
-		fmt.Printf("Test took %v\n", after_total.Sub(before_total))
-		s := 0
-		for _, succ := range successful {
-			s += succ
-		}
-
-		fmt.Printf("Successful: %d\n", s)
 	}
+	//connecting to servers
+	fmt.Printf("Connecting to replicas..\n")
+	servers := make([]net.Conn, N)
+	readers := make([]*bufio.Reader, N)
+	writers := make([]*bufio.Writer, N)
+	for i := 0; i < N; i++ {
+		var err error
+		servers[i], err = net.Dial("tcp", replicaList[i])
+		if err != nil {
+			fmt.Printf("Error connecting to replica %d\n", i)
+		}
+		fmt.Printf("Done connecting to %v\n", i)
+		readers[i] = bufio.NewReader(servers[i])
+		writers[i] = bufio.NewWriter(servers[i])
+	}
+	fmt.Printf("Connected to replicas: readers are %v .\n", readers)
+
+	successful = make([]int, N)
+	leader := 0
+	done := make(chan bool, N)
+	var id int32 = 0
+	args := clientproto.Propose{CommandId: id, Command: state.Command{Op: state.PUT, K: 0, V: 0}}
+	before_total := time.Now()
+	// sending requests
+	for j := 0; j < *rounds; j++ {
+
+		n := *reqsNb / *rounds
+		go waitReplies(readers, leader, n, done)
+		before := time.Now()
+		for i := 0; i < n; i++ {
+			dlog.Printf("Sending proposal %d with put %v, key %v and val %v \n", id, put[i], karray[i], state.Value(i))
+			args.CommandId = id
+			if put[i] {
+				args.Command.Op = state.PUT
+			} else {
+				args.Command.Op = state.GET
+			}
+			args.Command.K = state.Key(karray[i])
+			args.Command.V = state.Value(i)
+			writers[leader].WriteByte(clientproto.PROPOSE)
+			args.Marshal(writers[leader])
+			writers[leader].Flush()
+			id++
+		}
+		for i := 0; i < N; i++ {
+			writers[i].Flush()
+		}
+		err := <-done
+		after := time.Now()
+		fmt.Printf("Round took %v\n", after.Sub(before))
+		if err {
+			leader = (leader + 1) % N
+		}
+		id++
+
+	}
+	after_total := time.Now()
+	fmt.Printf("Test took %v\n", after_total.Sub(before_total))
+	s := 0
+	for _, succ := range successful {
+		s += succ
+	}
+
+	fmt.Printf("Successful: %d\n", s)
 }
 
 func waitReplies(readers []*bufio.Reader, leader int, n int, done chan bool) {
+	//fmt.Printf("Waiting for %v replies from %v\n", n, leader)
 	e := false
 
 	reply := new(clientproto.ProposeReply)
@@ -122,6 +124,7 @@ func waitReplies(readers []*bufio.Reader, leader int, n int, done chan bool) {
 			e = true
 			continue
 		}
+		//fmt.Printf("got reply #%v; ok: %v\n", i, reply.OK)
 		if reply.OK != 0 {
 			successful[leader]++
 		}
