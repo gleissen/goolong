39855ab5a5f76f4ad6c244e6d3da3777ec170a72
Klaus v. Gleissenthall <gleissen@ucsd.edu>
2018-10-07 18:23:12 -0700

    regression

--- src/paxos/paxos.go
+++ src/paxos/paxos.go
@@ -206,7 +206,7 @@ func runProposer(peerAddresses []string, termArg *int, proposalArg *int, done ch
 			              )
 					-@}*/
 
-			/*{-@pre: forall([decl(i,int), decl(j,int)],
+			/*{-@pre: forall([decl(i,int),decl(j,int)],
 			                          implies(
 																	and([
 																		elem(i,ps),
@@ -217,11 +217,9 @@ func runProposer(peerAddresses []string, termArg *int, proposalArg *int, done ch
 			                            ref(wT,j) < ref(t,i)
 																)
 													)
-			-@}*/
+						-@}*/
 
-			// main invariant
-
-			/*{-@pre: forall([decl(qa,int), decl(qp,int)],
+			/*{-@pre: forall([decl(qa,int),decl(qp,int)],
 			                          implies(
 																	and([
 																		elem(qa,as),
@@ -235,11 +233,10 @@ func runProposer(peerAddresses []string, termArg *int, proposalArg *int, done ch
 													)
 			-@}*/
 
-			// Nodes have disjoint tickets
-
 			//{-@ assume: forall([decl(i,int)], implies(ref(t,i)=ref(t,P), i=P)) -@}
 
-			// This fact derived from the cardility invariant over the proposal phase
+			// these facts are derived from cardinalities
+
 			/*{-@assume: forall([decl(i,int)],
 					implies(
 						and([
@@ -377,29 +374,29 @@ func runAcceptor(peerAddresses []string) {
 }
 
 /*{-@ ensures: forall([
+									decl(a0,int),
 									decl(p1,int),
 									decl(p2,int)
 									],
                   implies(
                       and([
-											  elem(a0,as),
+												elem(a0,as),
                         elem(p1,ps),
                         elem(p2,ps),
                         ref(decided,p1)=1,
                         ref(decided,p2)=1,
-                        implies(
-														and([
-																	ref(k,p1) > card(as)/2,
-                                  ref(k,p2) > card(as)/2
-															 ]),
-                            and([
-																	ref(t,p1) =< ref(wT,a0),
-																	ref(t,p2) =< ref(wT,a0)
-														    ])
-													),
-                          0 =< ref(l, p1),
-													0 =< ref(l ,p2)
-											  ]),
-                        ref(x,p1) = ref(x,p2))
+                              implies(and([
+																					ref(k,p1) > card(as)/2,
+                                    			ref(k,p2) > card(as)/2
+																			]),
+                                    	and([
+																					ref(t,p1) =< ref(wT,a0),
+																					ref(t,p2) =< ref(wT,a0)
+																				])
+															),
+                              0 =< ref(l, p1),
+															0 =< ref(l ,p2)
+															]),
+                              ref(x,p1) = ref(x,p2))
 									)
 	-@}*/
